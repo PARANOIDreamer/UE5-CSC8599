fc4b6edbc11c86af9e6b3c7b92b91f2c1a28c0ff
Rebecca <145792204+PARANOIDreamer@users.noreply.github.com>
2024-05-30 13:21:52 +0100

    Add pin 5-30

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialShader.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialShader.cpp
@@ -114,6 +114,11 @@ FString GetShadingModelString(EMaterialShadingModel ShadingModel)
 		case MSM_Eye:				ShadingModelName = TEXT("MSM_Eye"); break;
 		case MSM_SingleLayerWater:	ShadingModelName = TEXT("MSM_SingleLayerWater"); break;
 		case MSM_ThinTranslucent:	ShadingModelName = TEXT("MSM_ThinTranslucent"); break;
+
+		//My-Add-30/05/24
+		case MSM_Sketch: ShadingModelName = TEXT("MSM_Sketch"); break;
+		//End
+
 		default: ShadingModelName = TEXT("Unknown"); break;
 	}
 	return ShadingModelName;
@@ -248,7 +253,11 @@ void UpdateMaterialShaderCompilingStats(const FMaterial* Material)
 	{
 		INC_DWORD_STAT_BY(STAT_ShaderCompiling_NumUnlitMaterialShaders, 1);
 	}
-	else if (ShadingModels.HasAnyShadingModel({ MSM_DefaultLit, MSM_Subsurface, MSM_PreintegratedSkin, MSM_ClearCoat, MSM_Cloth, MSM_SubsurfaceProfile, MSM_TwoSidedFoliage, MSM_SingleLayerWater, MSM_ThinTranslucent }))
+	else if (ShadingModels.HasAnyShadingModel({ MSM_DefaultLit, MSM_Subsurface, MSM_PreintegratedSkin, MSM_ClearCoat, MSM_Cloth, MSM_SubsurfaceProfile, MSM_TwoSidedFoliage, MSM_SingleLayerWater, MSM_ThinTranslucent,
+		//My-Add-30/05/24
+		MSM_Sketch
+		//End
+		}))
 	{
 		INC_DWORD_STAT_BY(STAT_ShaderCompiling_NumLitMaterialShaders, 1);
 	}
